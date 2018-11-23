7f36874a827776b749ea34e9bd345d03ac32def8
Remi Boulier <boulier.r.job@gmail.com>
2018-11-23 04:41:55 +0000

    Upgrade getItemViewType tests

--- app/src/test/java/com/remiboulier/mychat/ChatListAdapterTest.kt
+++ app/src/test/java/com/remiboulier/mychat/ChatListAdapterTest.kt
@@ -1,7 +1,12 @@
 package com.remiboulier.mychat
 
+import android.content.Context
+import junit.framework.Assert.assertEquals
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
 
 /**
  * Created by Remi BOULIER on 21/11/2018.
@@ -10,23 +15,39 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class ChatListAdapterTest {
 
-//    @Test
-//    fun getItemViewType_when_even() {
-//        val context = mock(Context::class.java)
-//        val adapter = ChatListAdapter(mutableListOf("A", "B", "C"), context)
-//
-//        val res = adapter.getItemViewType(0)
-//
-//        assertEquals(ChatListAdapter.TYPE_USER, res)
-//    }
-//
-//    @Test
-//    fun getItemViewType_when_odd() {
-//        val context = mock(Context::class.java)
-//        val adapter = ChatListAdapter(mutableListOf("A", "B"), context)
-//
-//        val res = adapter.getItemViewType(1)
-//
-//        assertEquals(ChatListAdapter.TYPE_CONTACT, res)
-//    }
+    @Test
+    fun getItemViewType_is_user() {
+        val item = Message("Text", Date(), true, false)
+        val adapter = ChatListAdapter(
+                mutableListOf(mock(Item::class.java), item),
+                mock(Context::class.java))
+
+        val res = adapter.getItemViewType(1)
+
+        assertEquals(ChatListAdapter.TYPE_USER, res)
+    }
+
+    @Test
+    fun getItemViewType_is_contact() {
+        val item = Message("Text", Date(), false, false)
+        val adapter = ChatListAdapter(
+                mutableListOf(mock(Item::class.java), item),
+                mock(Context::class.java))
+
+        val res = adapter.getItemViewType(1)
+
+        assertEquals(ChatListAdapter.TYPE_CONTACT, res)
+    }
+
+    @Test
+    fun getItemViewType_is_separator() {
+        val item = TimeSeparator(Date())
+        val adapter = ChatListAdapter(
+                mutableListOf(mock(Item::class.java), item),
+                mock(Context::class.java))
+
+        val res = adapter.getItemViewType(1)
+
+        assertEquals(ChatListAdapter.TYPE_SEPARATOR, res)
+    }
 }
